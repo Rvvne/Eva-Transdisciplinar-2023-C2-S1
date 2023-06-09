d2681f01b48818d9c24c190162046d22cbcd3fb8
nvchowv <jcurihuinca2023@alu.uct.cl>
2023-06-08 23:36:48 -0400

    Cambio de interfaz
    Amplié la interfaz le cambie el color y cambie de posición algunos botones

--- pro y fisica.py
+++ pro y fisica.py
@@ -13,8 +13,9 @@ ventana = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Caida libre")
 
 # Cambia el color de la ventana
-color1 = 100, 100, 100
-color2 = 0, 0, 0
+color1= 100, 100, 100
+color2= 0, 0, 0
+color3= 210, 180, 140
 
 # Crear una ventana de Tkinter oculta para los botones
 root = tk.Tk()
@@ -26,27 +27,35 @@ def show_message_box():
 
 # Definir las dimensiones de las secciones
 seccion1_ancho = ancho // 2
-seccion2_ancho = ancho // 2
-seccion_alto = alto
+seccion2_ancho = 350
+alto1= 350
 
 # Crear superficies para las secciones
-lado_iz = pygame.Surface((seccion1_ancho, seccion_alto))
-lado_der = pygame.Surface((seccion2_ancho, seccion_alto))
+lado_iz = pygame.Surface((seccion1_ancho, alto))
+lado_der = pygame.Surface((seccion2_ancho, alto1))
 
-# Función para dibujar los botones en la sección 1
-def dibujar_botones1():
+# Bucle principal del juego
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    # Cambiar el color de fondo de las secciones
+    lado_iz.fill(color3)
+    lado_der.fill(color3)
+
     # Crear un rectángulo para el botón en la sección 1
-    boton1 = pygame.Rect(100, 100, 200, 50)
+    boton1= pygame.Rect(50, 150, 100, 50)
     pygame.draw.rect(lado_iz, (255, 255, 255), boton1)
-    
-    boton2 = pygame.Rect(100, 200, 200, 50)
-    pygame.draw.rect(lado_iz, (255, 255, 255), boton2)
-    if boton2.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
-        show_message_box()
 
+        
+    boton2= pygame.Rect(200,150 , 100, 50)
+    pygame.draw.rect(lado_iz, (255, 255, 255), boton2)
     
     # Obtener las coordenadas del ratón
     mouse_pos = pygame.mouse.get_pos()
+
 
     # Verificar si el ratón está sobre el botón en la sección 1 y si se hace clic
     if boton1.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]:
@@ -70,34 +79,22 @@ def dibujar_botones1():
         # Ejecutar el bucle principal de Tkinter
         ventana_tk.mainloop()
 
-# Función para actualizar la pantalla
-def actualizar_pantalla():
+    # Obtener las coordenadas del ratón
+    mouse_pos = pygame.mouse.get_pos()
+
+    # Verificar si el ratón está sobre el botón en la sección 1 y si se hace clic
+    if boton1.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]:
+        show_message_box()
+    if boton2.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]:
+        show_message_box()
+
+    # Copiar las superficies de las secciones en la ventana principal
     ventana.blit(lado_iz, (0, 0))
-    ventana.blit(lado_der, (seccion1_ancho, 0))
+    ventana.blit(lado_der,(seccion1_ancho, alto1))
+
+    # Actualizar la pantalla
     pygame.display.flip()
 
-# Bucle principal del juego
-def main_loop():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-
-        # Cambiar el color de fondo de las secciones
-        lado_iz.fill(color1)
-        lado_der.fill(color2)
-
-        # Dibujar los botones en cada sección
-        dibujar_botones1()
-
-        # Actualizar la pantalla
-        actualizar_pantalla()
-
-    # Cerrar pygame y Tkinter al finalizar
-    pygame.quit()
-    root.destroy()
-
-# Ejecutar el bucle principal
-if __name__ == "__main__":
-    main_loop()
+# Cerrar pygame y Tkinter al finalizar
+pygame.quit()
+root.destroy()
